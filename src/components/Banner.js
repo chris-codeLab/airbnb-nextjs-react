43a7ac35a4b521077eea1b811e742d93b79ecdb9
Chris Mason <96922999+msknDev@users.noreply.github.com>
2023-03-07 17:25:39 -0800

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
         alt='banner'
         />
         <div className='absolute top-1/2 w-full text-center'>
-            <p className='text-sm sm:text-lg text-black'>Hi Mrs. Mason...you are sexy as fuck! </p>
+            <p className='text-sm sm:text-lg text-black'>Not sure where to go? Perfect.</p>
             <button className='text-red-600 bg-white px-10 py-3 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-95 transition duration-150'>I'm flexible</button>
         </div>
     </div>
